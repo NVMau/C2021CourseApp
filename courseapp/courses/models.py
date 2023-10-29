4a73095b0d569b83e34f45ab8ccc19b6fb93dd7e
BEOBUJ <trackmix115@gmail.com>
2023-10-29 15:05:43 +0700

    TH lan 1

---
+++ courseapp/courses/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
+
+# Create your models here.
+class BaseModel(models.Model):
+    created_date = models.DateField(auto_now_add=True,null=True)
+    update_date = models.DateField(auto_now=True,null=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+class Category(BaseModel):
+    name = models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return  self.name
+
+class Course(BaseModel):
+    subject = models.CharField(max_length=255, null= False)
+    description = models.TextField()
+    image = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def  __str__(self):
+        return self.subject
